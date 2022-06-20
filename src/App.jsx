748d85dcd347b50c8a0a7f232fc083ad1f717cef
Mayank Chanabhatti <64948640+imickey04@users.noreply.github.com>
2022-06-21 00:47:15 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useRef, useState, useEffect } from 'react';
+
+function App() {
+
+  const videoRef = useRef("");
+  const photoRef = useRef("");
+
+  const [hasPhoto, setHasPhoto] = useState(false);
+
+  const getVideo = () => {
+    
+    navigator.mediaDevices.getUserMedia({ video: 
+      { width : 1920, height : 1080 } })
+      .then (stream => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
+
+  const takePhoto = ()=> {
+    const width = 414;
+    const height = width / (16/9);
+
+    let video = videoRef.current;
+    let photo = photoRef.current;
+
+    photo.width = width;
+    photo.height = height;
+
+    let ctx = photo.getContext('2d');
+    ctx.drawImage(video, 0, 0, width, height);
+
+    setHasPhoto(true);
+  }
+
+  const closePhoto = () => {
+    let photo = photoRef.current;
+    let ctx = photo.getContext('2d');
+    setHasPhoto(false);
+    ctx.clearRect(0, 0, photo.width, photo.height)
+  }
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef])
+
+  return (
+    <div className="App">
+      <div className="camera">
+        <video ref={videoRef}></video>
+        <button onClick={takePhoto}>Snap It!</button>
+      </div>
+
+      <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+        <canvas ref={photoRef}></canvas>
+        <button onClick={closePhoto}>Close!</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
